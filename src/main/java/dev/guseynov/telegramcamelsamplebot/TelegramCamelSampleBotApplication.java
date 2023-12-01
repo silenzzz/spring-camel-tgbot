61ce410a58c4b8bc0134fe74087b552c3de8819b
DeMmAge <34381399+DeMmAge@users.noreply.github.com>
2023-12-01 12:30:46 +0300

    Initial commit

---
+++ src/main/java/dev/guseynov/telegramcamelsamplebot/TelegramCamelSampleBotApplication.java
@@ -0,0 +1,13 @@
+package dev.guseynov.telegramcamelsamplebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramCamelSampleBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramCamelSampleBotApplication.class, args);
+    }
+
+}
